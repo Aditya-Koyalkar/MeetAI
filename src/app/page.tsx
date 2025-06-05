5b7f6c88387d2d02f40f464ca2b0c4378773f359
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-06-05 12:35:52 +0530

    auth modules sign in and sign up with better auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,4 @@
-"use client";
-
-import { useState } from "react";
-
 export default function Home() {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
   return (
     <div>
       <form></form>
