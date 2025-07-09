34c67af9fca057e5ea0f8aa025e45be3e5161fca
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-07-09 17:39:26 +0530

    all meeting status ui flow

--- src/components/empty-state.tsx
+++ src/components/empty-state.tsx
@@ -4,13 +4,14 @@ import Image from "next/image";
 type Props = {
   title: string;
   description: string;
+  image: string;
 };
 
-const EmptyState = ({ description, title }: Props) => {
+const EmptyState = ({ description, title, image = "/empty.svg" }: Props) => {
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-y-6 bg-background rounded-lg p-10 shadow-sm">
-        <Image src={"/logo.svg"} alt="Empty" width={240} height={240} />
+        <Image src={image} alt="Empty" width={240} height={240} />
         <div className="flex flex-col gap-y-2 text-center">
           <h6 className="text-lg font-medium">{title}</h6>
           <p className="text-sm">{description}</p>
--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -7,6 +7,10 @@ import { toast } from "sonner";
 import { useConfirm } from "../../hooks/use-confirm";
 import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
 import { useState } from "react";
+import { UpcomingState } from "../components/upcoming-state";
+import { ActiveState } from "../components/active-state";
+import { CancelledState } from "../components/cancelled-state";
+import { ProcessingState } from "../components/processing-state";
 
 interface Props {
   meetingId: string;
@@ -38,12 +42,24 @@ export const MeetingIdView = ({ meetingId }: Props) => {
     }
     await removeMeeting.mutate({ id: meetingId });
   };
+
+  const isActive = data.status === "active";
+  const isUpcoming = data.status === "upcoming";
+  const isCancelled = data.status === "cancelled";
+  const isCompleted = data.status === "completed";
+  const isProcessing = data.status === "processing";
   return (
     <>
       <RemoveConfirmation />
       <UpdateMeetingDialog open={updateMeetingOpen} onOpenChange={setUpdateMeetingOpen} initialValues={data} />
       <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
         <MeetingIdHeader meetindId={meetingId} meetingName={data.name} onEdit={() => setUpdateMeetingOpen(true)} onRemove={handleRemoveMeeting} />
+
+        {isCancelled && <CancelledState />}
+        {isUpcoming && <UpcomingState meetingId={meetingId} onCancelMeeting={() => {}} isCancelling={false} />}
+        {isCompleted && <></>}
+        {isProcessing && <ProcessingState />}
+        {isActive && <ActiveState meetingId={meetingId} />}
       </div>
     </>
   );
