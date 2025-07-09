cf4f0a862f4e022af777964de6924a4ffe7c67d5
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-07-09 16:06:35 +0530

    update and delete meeting

---
+++ src/modules/meetings/ui/components/update-meeting-dialog.tsx
@@ -0,0 +1,26 @@
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+import { MeetingForm } from "./meeting-form";
+import { MeetingGetOne } from "../../types";
+
+type Props = {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  initialValues: MeetingGetOne;
+};
+
+export const UpdateMeetingDialog = ({ open, onOpenChange, initialValues }: Props) => {
+  return (
+    <ResponsiveDialog title="Edit Meeting" description="Edit the meeting details" open={open} onOpenChange={onOpenChange}>
+      Update Meeting form
+      <MeetingForm
+        onSuccess={(id?: string) => {
+          onOpenChange(false);
+        }}
+        onCancel={() => {
+          onOpenChange(false);
+        }}
+        initialValues={initialValues}
+      />
+    </ResponsiveDialog>
+  );
+};
---
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
+import { MeetingIdHeader } from "../components/meeting-id-header";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useConfirm } from "../../hooks/use-confirm";
+import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
+import { useState } from "react";
+
+interface Props {
+  meetingId: string;
+}
+
+export const MeetingIdView = ({ meetingId }: Props) => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const [updateMeetingOpen, setUpdateMeetingOpen] = useState(false);
+  const [RemoveConfirmation, confirmRemove] = useConfirm("Are you sure?", `This following action will remove this meeting`);
+  const { data } = useSuspenseQuery(trpc.meetings.getOne.queryOptions({ id: meetingId }));
+  const removeMeeting = useMutation(
+    trpc.meetings.delete.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(trpc.meetings.getMany.queryOptions({}));
+        router.push(`/meetings`);
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    })
+  );
+
+  const handleRemoveMeeting = async () => {
+    const ok = await confirmRemove();
+    if (!ok) {
+      return;
+    }
+    await removeMeeting.mutate({ id: meetingId });
+  };
+  return (
+    <>
+      <RemoveConfirmation />
+      <UpdateMeetingDialog open={updateMeetingOpen} onOpenChange={setUpdateMeetingOpen} initialValues={data} />
+      <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
+        <MeetingIdHeader meetindId={meetingId} meetingName={data.name} onEdit={() => setUpdateMeetingOpen(true)} onRemove={handleRemoveMeeting} />
+      </div>
+    </>
+  );
+};
